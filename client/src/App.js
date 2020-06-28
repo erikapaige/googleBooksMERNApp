5b63e86ec78f90a39cec0cfc9cd80cd63658f11c
Erika <erika.paige.handley@gmail.com>
2020-06-28 14:26:00 -0700

    basic react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
+// bring in react
 import React from 'react'
+// bring in react router
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route
+} from 'react-router-dom'
 
 const App = () =>{
   return(
-    <div></div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/'>
+
+          </Route>
+          <Route exact path='/saved'>
+            
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
